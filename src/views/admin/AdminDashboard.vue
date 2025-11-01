7cf8296c187163d5c074f2a06b8e84f10b6df142
Elvis Izuegbu <izuegbuelvis@gmail.com>
2025-11-01 18:08:41 +0100

    refactored. added <main> landmark

--- src/views/admin/AdminDashboard.vue
+++ src/views/admin/AdminDashboard.vue
@@ -1,31 +1,27 @@
 <template>
-  <main class="w-full px-6 py-10 transition-all duration-300">
-    <div class="max-w-[1200px]">
-      <section class="flex items-center gap-3 mb-6">
-        <h2 class="text-lg sm:text-xl font-semibold">Dashboard Overview</h2>
-      </section>
+  <section class="flex items-center gap-3 mb-6">
+    <h2 class="text-lg sm:text-xl font-semibold">Dashboard Overview</h2>
+  </section>
 
-      <!-- Dashboard Cards -->
-      <section class="grid gap-4 sm:gap-6 grid-cols-1 sm:grid-cols-2 xl:grid-cols-3">
-        <div
-          v-for="card in cards"
-          :key="card.title"
-          class="bg-surface border border-border rounded-2xl shadow hover:shadow-md transition p-5 sm:p-6"
-        >
-          <div class="flex items-center justify-between">
-            <h3 class="text-base sm:text-lg font-semibold">
-              {{ card.title }}
-            </h3>
-            <component :is="card.icon" class="text-accent text-xl sm:text-2xl" />
-          </div>
-          <p class="text-2xl sm:text-3xl font-bold text-accent mt-3">
-            {{ card.value }}
-          </p>
-          <p class="text-muted text-sm mt-1">{{ card.desc }}</p>
-        </div>
-      </section>
+  <!-- Dashboard Cards -->
+  <section class="grid gap-4 sm:gap-6 grid-cols-1 sm:grid-cols-2 xl:grid-cols-3">
+    <div
+      v-for="card in cards"
+      :key="card.title"
+      class="bg-surface border border-border rounded-2xl shadow hover:shadow-md transition p-5 sm:p-6"
+    >
+      <div class="flex items-center justify-between">
+        <h3 class="text-base sm:text-lg font-semibold">
+          {{ card.title }}
+        </h3>
+        <component :is="card.icon" class="text-accent text-xl sm:text-2xl" />
+      </div>
+      <p class="text-2xl md:text-3xl font-bold text-accent mt-3">
+        {{ card.value }}
+      </p>
+      <p class="text-muted text-sm mt-1">{{ card.desc }}</p>
     </div>
-  </main>
+  </section>
 </template>
 
 <script setup>
